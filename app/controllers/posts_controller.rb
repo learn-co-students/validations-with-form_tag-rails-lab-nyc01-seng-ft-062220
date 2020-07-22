fc1d45b7cf2a4b603a7e43184c82cfd1f9829fba
Aiannn <soktoevayan@gmail.com>
2020-07-22 12:40:00 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,32 @@
 class PostsController < ApplicationController
+  before_action :set_find 
+  
   def show
-    @post = Post.find(params[:id])
+    set_find 
   end
 
   def edit
-    @post = Post.find(params[:id])
+    set_find 
   end
 
   def update
-    @post = Post.find(params[:id])
+    set_find 
 
-    @post.update(post_params)
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end 
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_find 
+    @post = Post.find(params[:id])
   end
 end
